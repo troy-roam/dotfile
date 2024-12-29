54bd5d1878690b667960f10c26abe31527f0f2ae
Troy <gnayoat@gmail.com>
2024-12-29 22:28:10 +0800

    add yazi

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 vim.opt.colorcolumn  = "80"
 vim.opt.nu             = false
-vim.wo.wrap            = false
+vim.wo.wrap            = true
 vim.opt.breakindent    = true
 vim.opt.breakindentopt = 'shift:0,min:40,sbr'
-vim.opt.linebreak      = false
-vim.opt.showbreak      = '>'
+vim.opt.linebreak      = true
+vim.opt.showbreak      = '> '
