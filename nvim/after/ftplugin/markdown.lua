dfe8d92d2a82ddb60a30c71a6ed34c528dc06f67
Troy <gnayoat@gmail.com>
2025-01-01 15:58:46 +0800

    asd

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 -- vim.opt.colorcolumn  = "80"
 vim.opt.nu             = false
-vim.opt.wrap           = false
+vim.opt.wrap           = true
 vim.opt.breakindent    = true
 vim.opt.breakindentopt = 'shift:0,min:40,sbr'
 vim.opt.linebreak      = true
