79fc386995e7e4fb23ccdcd84419458d528c070a
Troy <yangtao.678@bytedance.com>
2025-07-28 16:22:38 +0800

    disable s open third editor

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -3,6 +3,16 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
+    on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        vim.keymap.set('n', 's', '', { buffer = bufnr })
+        vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+        -- vim.keymap.set('n', 's', api.node.open.edit, opts('Open'))
+    end,
     git = {
         enable = false,
     },
