241e17ab6124fdc021c5ef3545b065df400860ec
Troy <yangtao.678@bytedance.com>
2025-07-29 19:23:20 +0800

    change

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -3,16 +3,16 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-    on_attach = function(bufnr)
-        local api = require("nvim-tree.api")
-        local function opts(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
+    -- on_attach = function(bufnr)
+    --     local api = require("nvim-tree.api")
+    --     local function opts(desc)
+    --         return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    --     end
 
-        vim.keymap.set('n', 's', '', { buffer = bufnr })
-        vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
-        -- vim.keymap.set('n', 's', api.node.open.edit, opts('Open'))
-    end,
+    --     vim.keymap.set('n', 's', '', { buffer = bufnr })
+    --     vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+    --     -- vim.keymap.set('n', 's', api.node.open.edit, opts('Open'))
+    -- end,
     git = {
         enable = false,
     },
@@ -29,8 +29,25 @@ require("nvim-tree").setup({
             inline_arrows = true,
         },
         icons = {
+            glyphs = {
+                default = "-",         -- 普通文件
+                symlink = "@",         -- 符号链接
+                folder = {
+                    arrow_closed = ">", -- 折叠箭头
+                    arrow_open   = "v", -- 展开箭头
+                    default      = "+", -- 普通文件夹
+                    open         = "-", -- 打开的文件夹
+                    empty        = "+", -- 空文件夹
+                    empty_open   = "-", -- 打开的空文件夹
+                    symlink = "@",      -- 符号链接文件夹
+                },
+            },
             show = {
+                file = true,
+                folder = true,
+                folder_arrow = false,
                 git = false,
+                diagnostics = false,
             },
  
         },
