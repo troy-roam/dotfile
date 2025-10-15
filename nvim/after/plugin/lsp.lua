e2f04ce96b6db37fa8c1353d78d74a60c1665b5b
Troy <yangtao.678@bytedance.com>
2025-10-15 15:07:35 +0800

    update

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,115 +1,125 @@
-require('lspconfig').rust_analyzer.setup({})
-require('lspconfig').ts_ls.setup({})
-require('lspconfig').pyright.setup({})
+-- ==============================
+--  Global LSP UI Setup
+-- ==============================
+local border = "double" -- "rounded", "single", "double", "shadow", "none"
 
-require('lspconfig').gopls.setup({
-    settings = {
-        gopls = {
-            gofumpt = true,
-            staticcheck = true,
-            -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md#diagnosticsdelay-timeduration
-            diagnosticsDelay = "3s",
-            completionBudget = "1s",
-            -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
-            analyses = {
-                unusedparams = true,
-            },
-        },
-    },
-    on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
-            callback = function()
-                local params = vim.lsp.util.make_range_params()
-                params.context = {only = {"source.organizeImports"}}
-                -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-                -- machine and codebase, you may want longer. Add an additional
-                -- argument after params if you find that you have to write the file
-                -- twice for changes to be saved.
-                -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-                local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-                for cid, res in pairs(result or {}) do
-                    for _, r in pairs(res.result or {}) do
-                        if r.edit then
-                            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-                            vim.lsp.util.apply_workspace_edit(r.edit, enc)
-                        end
-                    end
-                end
-                vim.lsp.buf.format({async = false})
-            end
-        })
+-- Floating window borders for hover/signature
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = border }
+)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  { border = border }
+)
 
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ async = false })
-            end,
-        })
-    end,
+-- Diagnostic window style
+vim.diagnostic.config({
+  float = { border = border },
 })
 
-require'lspconfig'.pylsp.setup{
+
+-- ==============================
+--  Global LSP Keymaps
+-- ==============================
+local function setup_global_lsp_keymaps()
+  local opts = { noremap = true, silent = true }
+
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover({ border = border })
+  end, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>ff", function()
+    vim.lsp.buf.format({ async = true })
+  end, opts)
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = setup_global_lsp_keymaps,
+})
+
+-- Diagnostics navigation
+vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
+
+
+-- ==============================
+--  LSP Server Configurations
+-- ==============================
+
+-- Rust
+vim.lsp.config.rust_analyzer = {}
+
+-- TypeScript / JavaScript
+vim.lsp.config.ts_ls = {}
+
+-- Python (pyright)
+vim.lsp.config.pyright = {}
+
+-- Python (pylsp)
+vim.lsp.config.pylsp = {
   settings = {
     pylsp = {
       plugins = {
         pycodestyle = {
-          ignore = {'W391'},
-          maxLineLength = 100
-        }
-      }
-    }
-  }
+          ignore = { "W391" },
+          maxLineLength = 100,
+        },
+      },
+    },
+  },
 }
 
--- require('lspconfig').lua_ls.setup {
---   settings = {
---     Lua = {
---       runtime = {
---         version = 'LuaJIT',
---       },
---       diagnostics = {
---         globals = { 'vim' },
---       },
---       workspace = {
---         library = vim.api.nvim_get_runtime_file("", true),
---         checkThirdParty = false,
---       },
---       telemetry = {
---         enable = false,
---       },
---     },
---   },
--- }
+-- Go (gopls)
+vim.lsp.config.gopls = {
+  settings = {
+    gopls = {
+      gofumpt = true,
+      staticcheck = true,
+      diagnosticsDelay = "3s",
+      completionBudget = "1s",
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+  on_attach = function(client, bufnr)
+    -- Autoformat and organize imports on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        -- organize imports
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        for cid, res in pairs(result or {}) do
+          for _, r in pairs(res.result or {}) do
+            if r.edit then
+              local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+              vim.lsp.util.apply_workspace_edit(r.edit, enc)
+            end
+          end
+        end
 
-local function setup_global_lsp_keymaps()
-    local opts = { noremap = true, silent = true }
+        -- format
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
+}
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format({ async = true }) end, opts)
-end
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        setup_global_lsp_keymaps()
-    end,
-})
-
-vim.api.nvim_set_keymap('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
-
-local border = "double" -- "rounded", "single", "double", "shadow", "none"
-vim.lsp.handlers["textDocument/hover"]         = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
-
-vim.diagnostic.config({
-    float = { border = border },
-})
+-- ==============================
+--  Enable All Servers
+-- ==============================
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("pyright")
+vim.lsp.enable("pylsp")
+vim.lsp.enable("gopls")
