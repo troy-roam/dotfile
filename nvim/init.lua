cb45addc73acf3eebf66e78b3f213676df832265
Troy <yangtao.678@bytedance.com>
2025-11-10 19:07:46 +0800

    add plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -82,6 +82,19 @@ require("lazy").setup({
         "junegunn/fzf.vim",
         dependencies = { "junegunn/fzf" },
         event = "VeryLazy"
+    },
+    -- https://github.com/kawre/leetcode.nvim
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+        dependencies = {
+            -- include a picker of your choice, see picker section for more details
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            -- configuration goes here
+        },
     }
 })
 
