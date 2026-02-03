70fd44b0e19ffadc7aa430cacc7c0fa42edccc07
troy <gnayoat@gmail.com>
2026-02-03 17:58:30 +0800

    x

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,4 +1,9 @@
-require'nvim-treesitter.configs'.setup {
+local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
+if not ok then
+    return
+end
+
+ts_configs.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "go" },
 
